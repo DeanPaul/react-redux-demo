54b453656eb466400b97133d527fc19073f4f19d
DeanPaul <83623378@qq.com>
2018-02-04 17:42:11 +0800

    simple react demo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-redux-demo
+react 入门，你可以通过[这个链接](https://github.com/DeanPaul/react-redux-demo/issues/1)一步一步完成一个简单的React App
+#####npm install
+#####npm run start
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                plugins: ['transform-runtime'],
+                presets: ['es2015', 'react', 'stage-2']
+            }
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    }
+};
+
