4879e134bfbbc17b8dab68eca6300d98325b67ee
DeanPaul <35568588+DeanPaul@users.noreply.github.com>
2018-02-07 15:36:26 +0800

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import moduleA from './moduleA';
 import moduleB from './moduleB';
+import count from './count';
 import {routerReducer} from 'react-router-redux';
 const rootReducer = combineReducers({
     moduleA,
     moduleB,
+    count,
     routing:routerReducer
 });
 
